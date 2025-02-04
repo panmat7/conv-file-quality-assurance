a3ef56ed3b7d5fbf22e73277a199ff2a601a3bed
pettersandvik0 <petter.sandvik3@gmail.com>
2025-02-04 17:08:17 +0100

    Added image to image color profile check

--- FileVerifier/src/ComparingMethods/ColorProfileComparison.cs
+++ FileVerifier/src/ComparingMethods/ColorProfileComparison.cs
@@ -1,4 +1,7 @@
-﻿using AvaloniaDraft.FileManager;
+﻿using System;
+using AvaloniaDraft.FileManager;
+using AvaloniaDraft.Helpers;
+using ImageMagick;
 
 namespace AvaloniaDraft.ComparingMethods;
 
@@ -11,11 +14,33 @@ public class ColorProfileComparison
     /// <returns> Returns whether it passed the test </returns>
     public static bool FileColorProfileComparison(FilePair files)
     {
+        var oFormat = files.OriginalFileFormat;
+        var nFormat = files.NewFileFormat;
+
+        try
+        {
+            return oFormat switch
+            {
+                _ when FormatCodes.PronomCodesImages.Contains(oFormat) && FormatCodes.PronomCodesImages.Contains(nFormat) 
+                    => ImageToImageColorProfileComparison(files.OriginalFilePath, files.NewFilePath),
+                _ when FormatCodes.PronomCodesImages.Contains(oFormat) && FormatCodes.PronomCodesPDF.Contains(nFormat)
+                    => ImageToPdfColorProfileComparison(files.OriginalFilePath, files.NewFilePath),
+                _ when FormatCodes.PronomCodesPDF.Contains(oFormat) && FormatCodes.PronomCodesPDF.Contains(nFormat) =>
+                    PdfToPdfColorProfileComparison(files.OriginalFilePath, files.NewFilePath),
+                _ => false
+            };
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error while comparing color profiles: {e.Message}");
+            return false; // If checking for color profile fails it automatically fails the test
+        }
+        
+    }
+    
+    public static bool ImageToImageColorProfileComparison(string originalImagePath, string newImagePath)
+    {
         //TODO
-        
-        // Check if the file formats are relevant for comparison criteria
-        
-        // Check if the files include images
         
         // Extract the images for each file
         
@@ -27,4 +52,67 @@ public class ColorProfileComparison
         
         return true;
     }
+    
+    public static bool PdfToPdfColorProfileComparison(string originalPdfPath, string newPdfPath)
+    {
+        //TODO
+        
+        // Extract the images for each file
+        
+        // Gather color profiles if present
+        
+        // Compare color profiles
+        
+        // Return result
+        
+        return true;
+    }
+    
+    public static bool ImageToPdfColorProfileComparison(string originalImagePath, string newPdfPath)
+    {
+        //TODO
+        
+        // Extract the images for each file
+        
+        // Gather color profiles if present
+        
+        // Compare color profiles
+        
+        // Return result
+        
+        return true;
+    }
+    
+    /// <summary>
+    /// Function checks that embedded color profile for two images are the same
+    /// </summary>
+    /// <param name="oPath"></param>
+    /// <param name="nPath"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public static bool CompareColorProfiles(string oPath, string nPath)
+    {
+        using var oImage = new MagickImage(oPath);
+        using var nImage = new MagickImage(nPath);
+        
+        var oProfile = oImage.GetColorProfile();
+        
+        if (oProfile == null) throw new Exception("Missing color profile embedded in original image");
+        
+        var nProfile = nImage.GetColorProfile();
+        
+        if (nProfile == null) throw new Exception("Missing color profile embedded in new image");
+        
+        return oProfile.Equals(nProfile);
+    }
 }
+
+// Check if the files include images
+        
+// Extract the images for each file
+        
+// Gather color profiles if present
+        
+// Compare color profiles
+        
+// Return result
