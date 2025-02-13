f892bcaafef4710276c7ef8b9a01bbd36d147589
Fredrik Grøttås <fredgrot@stud.ntnu.no>
2025-02-13 13:51:48 +0100

    Methods helper class

---
+++ FileVerifier/src/Helpers/Methods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaDraft.src.Helpers
+{    public struct Method
+    {
+        public string Name { get; }
+        public string Description { get; }
+
+        public Method(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+
+
+    /// <summary>
+    /// A helper class containing lists of comparison methods
+    /// </summary>
+    public static class Methods
+    {
+        public static Method Fonts => new Method("Fonts", "Check to ensure the documents use the same fonts.");
+        public static Method Pages => new Method("Pages", "Check to ensure the documents have the same amount of pages.");
+        public static Method Resolution => new Method("Resolution", "Check to ensure the resolution of the two images is the same.");
+        public static Method ColorSpace => new Method("Color space", "Check to ensure the documents use the same color space.");
+        public static Method Animations => new Method("Animations", "Check if the presentation has animations.");
+
+
+        /// <summary>
+        /// Get all the methods in a list
+        /// </summary>
+        public static List<Method> GetList()
+        {
+            var methods = new List<Method>();
+            var properties = typeof(Methods).GetProperties();
+            foreach (var p in properties)
+            {
+                var m = p.GetValue(null);
+                if (m is Method method) methods.Add(method);
+            }
+
+            return methods;
+        }
+    }
+
+}
