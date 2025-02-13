08d3d8801e1f4bfcc9c10b995b2ccab5785416c7
Fredrik Grøttås <fredgrot@stud.ntnu.no>
2025-02-13 13:56:32 +0100

    Fixed Methods namespace

--- FileVerifier/src/Helpers/Methods.cs
+++ FileVerifier/src/Helpers/Methods.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AvaloniaDraft.src.Helpers
+namespace AvaloniaDraft.Helpers
 {    public struct Method
     {
         public string Name { get; }
