e5c0e38c65d454fba032ef5534399ba165b1196a
pettersandvik0 <petter.sandvik3@gmail.com>
2025-03-04 18:05:38 +0100

    Added method to compare transparency in images and pdfs

--- FileVerifier/src/ComparingMethods/TransparencyComparison.cs
+++ FileVerifier/src/ComparingMethods/TransparencyComparison.cs
@@ -1,4 +1,9 @@
+using System.Linq;
 using AvaloniaDraft.FileManager;
+using ImageMagick;
+using SixLabors.ImageSharp;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.Tokens;
 
 namespace AvaloniaDraft.ComparingMethods;
 
@@ -11,16 +16,53 @@ public static class TransparencyComparison
     
     public static bool ImageToPdfTransparencyComparison(FilePair files)
     {
-        return true;
+        var oImage = new MagickImage(files.OriginalFilePath);
+        var nImage = ImageExtraction.GetNonDuplicatePdfImages(files.NewFilePath)[0];
+
+        return CheckNonPdfImageTransparency(oImage) == CheckPdfImageTransparency(nImage);
     }
     
     public static bool PdfToPdfTransparencyComparison(FilePair files)
     {
+        var oImages = ImageExtraction.GetNonDuplicatePdfImages(files.OriginalFilePath);
+        var nImages = ImageExtraction.GetNonDuplicatePdfImages(files.NewFilePath);
+
+        if (oImages.Count != nImages.Count)
+        {
+            return false;
+        }
+
+        for (var i = 0; i < oImages.Count; i++)
+        {
+            var oImage = oImages[i];
+            var nImage = nImages[i];
+
+            var oImageHasTransparency = CheckPdfImageTransparency(oImage);
+            var nImageHasTransparency = CheckPdfImageTransparency(nImage);
+
+            if (oImageHasTransparency != nImageHasTransparency)
+            {
+                return false;
+            }
+        }
         return true;
     }
     
     public static bool OdtAndOdpToPdfTransparencyComparison(FilePair files)
     {
         return true;
+    }
+
+    private static bool CheckPdfImageTransparency(IPdfImage image)
+    {
+        // Check for soft mask (SMask)
+        return image.ImageDictionary.ContainsKey(NameToken.Smask) ||
+               // Check for explicit mask (Mask)
+               image.ImageDictionary.ContainsKey(NameToken.Mask);
+    }
+
+    private static bool CheckNonPdfImageTransparency(MagickImage image)
+    {
+        return image.HasAlpha && image.GetPixels().Any(pixel => pixel.ToColor()!.A < 255);
     }
 }
