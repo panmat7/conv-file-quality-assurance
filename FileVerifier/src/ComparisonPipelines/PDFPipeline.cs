32144c19c206f36be16d74c6fd03293597917986
pettersandvik0 <petter.sandvik3@gmail.com>
2025-03-18 11:57:55 +0100

    Added transparency and color profile comparison to image and pdf pipelines

---
+++ FileVerifier/src/ComparisonPipelines/PDFPipeline.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using AvaloniaDraft.ComparingMethods;
+using AvaloniaDraft.FileManager;
+using AvaloniaDraft.Helpers;
+
+namespace AvaloniaDraft.ComparisonPipelines;
+
+public static class PdfPipeline
+{
+    /// <summary>
+    /// Function responsible for assigning the correct pipeline for PDF files
+    /// </summary>
+    /// <param name="outputFormat">Format of the converted file</param>
+    /// <returns>Function with the correct pipeline, null if there were no suitable function.</returns>
+    public static Action<FilePair, int, Action<int>, Action>? GetPdfPipelines(string outputFormat)
+    {
+        return null;
+    }
+    
+    /// <summary>
+    /// Pipeline responsible for comparing PDF to other PDF conversions
+    /// </summary>
+    /// <param name="pair">The pair of files to compare</param>
+    /// <param name="additionalThreads">Number of threads available for usage</param>
+    /// <param name="updateThreadCount">Callback function used to update current thread count</param>
+    /// <param name="markDone">Function marking the FilePair as done</param>
+    private static void PdfToPdfPipeline(FilePair pair, int additionalThreads, Action<int> updateThreadCount,
+        Action markDone)
+    {
+        BasePipeline.ExecutePipeline(() =>
+        {
+            List<Error> e = [];
+            
+            if (true) //Check options for file size check later
+            {
+                var res = ComperingMethods.GetFileSizeDifference(pair);
+                
+                var f = new FileInfo(pair.OriginalFilePath);
+                
+                if (res > f.Length * 0.5) //Adjust for accuracy later
+                {
+                    //For now only printing to console
+                    e.Add(new Error(
+                        "File Size Difference",
+                        "The difference in size for the two files exceeds expected values.",
+                        ErrorSeverity.High,
+                        ErrorType.FileError
+                    ));
+                }
+            }
+            
+            if (true) //Check options for metadata check later
+            {
+                var res = ComperingMethods.GetMissingOrWrongImageMetadataExif(pair);
+
+                if (res is null)
+                {
+                    e.Add(new Error(
+                        "Error getting image metadata",
+                        "There occured an error while trying to get metadata from one of the files.",
+                        ErrorSeverity.High,
+                        ErrorType.Metadata
+                    ));
+                } else if (res.Count > 0)
+                {
+                    e.AddRange(res);
+                }
+            }
+            
+            if (true) // Check for color profile later
+            {
+                var res = false;
+                var exceptionOccurred = false;
+
+                try
+                {
+                    res = ColorProfileComparison.PdfToPdfColorProfileComparison(pair);
+                }
+                catch (Exception)
+                {
+                    exceptionOccurred = true;
+                    e.Add(new Error(
+                        "Error comparing color profiles in pdf contained images",
+                        "There occurred an error while extracting and comparing " +
+                        "color profiles of the images contained in the pdf.",
+                        ErrorSeverity.High,
+                        ErrorType.Metadata
+                    ));
+                }
+
+                if (!exceptionOccurred && !res)
+                {
+                    e.Add(new Error(
+                        "Difference in images contained in the pdf's color profile",
+                        "The images contained in the two pdfs did not pass Color Profile comparison.",
+                        ErrorSeverity.Medium,
+                        ErrorType.Metadata
+                    ));
+                }
+            }
+            
+            if (true) // Check for transparency later
+            {
+                var res = false;
+                var exceptionOccurred = false;
+
+                try
+                {
+                    res = TransparencyComparison.PdfToPdfTransparencyComparison(pair);
+                }
+                catch (Exception)
+                {
+                    exceptionOccurred = true;
+                    e.Add(new Error(
+                        "Error comparing transparency in pdf contained images",
+                        "There occurred an error while comparing transparency" +
+                        " of the images contained in the pdf.",
+                        ErrorSeverity.High,
+                        ErrorType.Metadata
+                    ));
+                }
+
+                if (!exceptionOccurred && !res)
+                {
+                    e.Add(new Error(
+                        "Difference in images contained in the pdf's transparency",
+                        "The images contained in the two pdfs did not pass Transparency comparison.",
+                        ErrorSeverity.Medium,
+                        ErrorType.Visual
+                    ));
+                }
+            }
+            
+            ConsoleService.Instance.WriteToConsole(
+                $"Result for {Path.GetFileName(pair.OriginalFilePath)}-{Path.GetFileName(pair.NewFilePath)} Comparison: \n" +
+                e.GenerateErrorString() + "\n\n");
+            
+        }, additionalThreads, updateThreadCount, markDone);
+    }
+}
