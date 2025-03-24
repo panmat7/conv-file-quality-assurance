01854823163445eef06a253735ee037d8427bb92
pettersandvik0 <petter.sandvik3@gmail.com>
2025-03-24 14:38:37 +0100

    Fixed odp animation check

--- UnitTests/ComparingMethodsTest/AnimationComparisonTest.cs
+++ UnitTests/ComparingMethodsTest/AnimationComparisonTest.cs
@@ -1,23 +1,6 @@
 using AvaloniaDraft.ComparingMethods;
-using AvaloniaDraft.FileManager;
-using AvaloniaDraft.Helpers;
 
 namespace UnitTests.ComparingMethodsTest;
-
-[TestFixture]
-public class IsPowerPointFileTests
-{
-    [TestCase("fmt/215", true)]  // Valid PowerPoint format
-    [TestCase("fmt/126", true)]  // Valid PowerPoint format
-    [TestCase("fmt/123", false)] // Invalid format
-    [TestCase("", false)]        // Empty string
-    [TestCase(null, false)]      // Null
-    public void TestIsPowerPointFile(string? format, bool expected)
-    {
-        var result = format != null && FormatCodes.PronomCodesPresentationDocuments.Contains(format);
-        Assert.That(result, Is.EqualTo(expected));
-    }
-}
 
 [TestFixture]
 public class CheckOdpFormatForAnimationTests : TestBase
@@ -38,77 +21,6 @@ public class CheckOdpFormatForAnimationTests : TestBase
         
         var result = AnimationComparison.CheckOdpForAnimation(filePath);
         Assert.That(result, Is.False); // Both PowerPoint files should pass
-    }
-}
-
-[TestFixture]
-public class FileAnimationComparisonTests : TestBase
-{
-    [Test]
-    public void TestNonPowerPointFile()
-    {
-        var filePath = Path.Combine(TestFileDirectory, "PowerPoint", "non_powerpoint_file.txt");
-        var files = new FilePair(filePath, "x-fmt/111", filePath, "x-fmt/111");
-        var result = AnimationComparison.FileAnimationComparison(files);
-        Assert.That(result, Is.True); // Non-PowerPoint file should pass
-    }
-    
-    [Test]
-    public void TestBothPowerPointFiles()
-    {
-        var oFilePath = Path.Combine(TestFileDirectory, "PowerPoint", "presentation_with_animations.ppt");
-        var nFilePath = Path.Combine(TestFileDirectory, "PowerPoint", "presentation_with_animations.pptx");
-        var files = new FilePair(oFilePath, "fmt/126", nFilePath, "fmt/215");
-        var result = AnimationComparison.FileAnimationComparison(files);
-        Assert.That(result, Is.True); // Both PowerPoint files should pass
-    }
-    
-    [Test]
-    public void TestInvalidOriginalFilePath()
-    {
-        var nFilePath = Path.Combine(TestFileDirectory, "PDF", "presentation_with_animations.pdf");
-        var files = new FilePair("invalid_path.pptx", "fmt/215", nFilePath, "fmt/19");
-        var result = AnimationComparison.FileAnimationComparison(files);
-        Assert.That(result, Is.False); // Invalid file path should fail
-    }
-
-    [Test]
-    public void TestInvalidNewFilePath()
-    {
-        var oFilePath = Path.Combine(TestFileDirectory, "PowerPoint", "presentation_with_animations.ppt");
-        var files = new FilePair(oFilePath, "fmt/126", "invalid_path.pdf", "fmt/19");
-        var result = AnimationComparison.FileAnimationComparison(files);
-        Assert.That(result, Is.False); // Invalid new file path should fail
-    }
-    
-    [Test]
-    public void TestPowerPointFileWithAnimations()
-    {
-        var oFilePath = Path.Combine(TestFileDirectory, "PowerPoint", "presentation_with_animations.pptx");
-        var nFilePath = Path.Combine(TestFileDirectory, "PDF", "presentation_with_animations.pdf");
-        var files = new FilePair(oFilePath, "fmt/215", nFilePath, "fmt/19");
-        var result = AnimationComparison.FileAnimationComparison(files);
-        Assert.That(result, Is.False); // PowerPoint file with animations should fail
-    }
-
-    [Test]
-    public void TestPowerPointFileWithoutAnimations()
-    {
-        var oFilePath = Path.Combine(TestFileDirectory, "PowerPoint", "presentation_without_animations.pptx");
-        var nFilePath = Path.Combine(TestFileDirectory, "PDF", "presentation_without_animations.pdf");
-        var files = new FilePair(oFilePath, "fmt/215", nFilePath, "fmt/19");
-        var result = AnimationComparison.FileAnimationComparison(files);
-        Assert.That(result, Is.True); // PowerPoint file without animations should pass
-    }
-    
-    [Test]
-    public void TestOlderPowerPointFileWithAnimations()
-    {
-        var oFilePath = Path.Combine(TestFileDirectory, "PowerPoint", "presentation_with_animations.ppt");
-        var nFilePath = Path.Combine(TestFileDirectory, "PDF", "presentation_with_animations.pdf");
-        var files = new FilePair(oFilePath, "fmt/126", nFilePath, "fmt/19");
-        var result = AnimationComparison.FileAnimationComparison(files);
-        Assert.That(result, Is.False); // PowerPoint file with animations should fail
     }
 }
 
